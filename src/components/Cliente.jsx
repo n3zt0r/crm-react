29cfb44823fb82b19e7a2b37c3f119b7f8632742
Nestor Lopez <n3zt0r77@gmail.com>
2024-02-27 16:33:30 -0600

    NavBar and Client Page created

---
+++ src/components/Cliente.jsx
@@ -0,0 +1,43 @@
+function Cliente({ cliente }) {
+  const { id, nombre, telefono, email, empresa } = cliente;
+
+  return (
+    <tr className="border-b">
+      <td className="p-6 space-y-2">
+        <p className="text-2xl text-gray-800">{nombre}</p>
+        <p>{empresa}</p>
+      </td>
+
+      <td className="p-6">
+        <p className="text-gray-600">
+          <span className="text-gray-800 uppercase font-bold">Email: </span>
+          {email}
+        </p>
+        <p className="text-gray-600">
+          <span className="text-gray-800 uppercase font-bold">Tel: </span>
+          {telefono}
+        </p>
+      </td>
+
+      <td className="p-6 flex gap-3">
+        <button
+          type="button"
+          className="text-blue-600 hover:text-blue-700 uppercase font-bold text-sm"
+        >
+          Editar
+        </button>
+
+        <button
+          type="button"
+          className="text-red-600 hover:text-red-700 uppercase font-bold text-sm"
+        >
+          Eliminar
+        </button>
+      </td>
+    </tr>
+  );
+}
+
+Cliente.propTypes = String;
+
+export default Cliente;
---
+++ src/pages/NuevoCliente.jsx
@@ -0,0 +1,12 @@
+function NuevoCliente() {
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
+      <p className="mt-10">
+        Llena todos los campos para registrar un nuevo cliente
+      </p>
+    </>
+  );
+}
+
+export default NuevoCliente;
